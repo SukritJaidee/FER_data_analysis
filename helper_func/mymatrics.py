8721b936424557da359fba58b80b252aa9cb214a
Sukrit Jaidee <41770170+SukritJaidee@users.noreply.github.com>
2023-02-12 15:03:03 +0700

    Update mymatrics.py

--- helper_func/mymatrics.py
+++ helper_func/mymatrics.py
@@ -55,3 +55,27 @@ def f1_m(y_true, y_pred):
 # res = f1_m(y_true, y_pred)
 # print(res.numpy())
 
+
+## @title tfa.metrics.F1Score (# Multilabel)
+
+# metric = tfa.metrics.F1Score(num_classes=5, threshold=0.5)
+# y_true = np.array([[0, 1, 2, 0, 1]], np.int32)
+# y_pred = np.array([[0, 2, 1, 0, 0]], np.float32)
+# metric.update_state(y_true, y_pred)
+# result = metric.result()
+# res1 = result.numpy()
+# res2 = res1.mean()
+# print(f"tfa res1 {res1} res2 {res2}")
+
+# metric = tfa.metrics.F1Score(num_classes=3, threshold=0.5)
+# y_true = np.array([[1, 1, 1],
+#                             [1, 0, 0],
+#                             [1, 1, 0]], np.int32)
+# y_pred = np.array([[0.2, 0.6, 0.7],
+#                             [0.2, 0.6, 0.6],
+#                             [0.6, 0.8, 0.0]], np.float32)
+# metric.update_state(y_true, y_pred)
+# result = metric.result()
+# res1 = result.numpy()
+# res2 = res1.mean()
+# print(f"tfa res1 {res1} res2 {res2}")
