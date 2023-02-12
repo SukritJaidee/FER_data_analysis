618f7e1ce4d5ebbdd3f62b64eca185551029719f
Sukrit Jaidee <41770170+SukritJaidee@users.noreply.github.com>
2023-02-12 15:22:31 +0700

    Update mymatrics.py

--- helper_func/mymatrics.py
+++ helper_func/mymatrics.py
@@ -79,3 +79,42 @@ def f1_m(y_true, y_pred):
 # res1 = result.numpy()
 # res2 = res1.mean()
 # print(f"tfa res1 {res1} res2 {res2}")
+
+## section4
+#@title f1
+def f1(y_true, y_pred):
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
+
+def f1_loss(y_true, y_pred):
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+    return 1 - K.mean(f1)
+
+# # y_true = np.array([[0, 1, 2, 0, 1]])
+# # y_pred = np.array([[0, 2, 1, 0, 0]])
+# y_true = np.array([[1, 1, 1], [1, 0, 0], [1, 1, 0]], np.int32)
+# y_pred = np.array([[0.2, 0.6, 0.7], [0.2, 0.6, 0.6], [0.6, 0.8, 0.0]], np.float32)
+# res1 = f1(y_true, y_pred)
+# res2 = res1.numpy()
+# print(f"MultilabelF1Score: {res2}")
+# res1 = f1_loss(y_true, y_pred)
+# print(f"f1 loss: {res1}")
+
+
+
+
